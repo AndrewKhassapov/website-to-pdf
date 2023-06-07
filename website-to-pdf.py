4a71f1af0757309fd3b8e40aac51020f1a1067dc
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-06-07 14:53:06 +1000

    Update website-to-pdf.py
    + Added directory creating. The directory must otherwise exist to save files to it.
    + Confirmed end-to-end working get_url_list_from_site()
    TODO: Create read file to list.
    TODO: Parse desired websites.

--- website-to-pdf.py
+++ website-to-pdf.py
@@ -1,12 +1,10 @@
 import pdfkit
 import time
 import requests
+import os
 from bs4 import BeautifulSoup
 
-urls_to_parse = [
-    "https://howmuch.water.vic.gov.au/",
-    "https://howmuch.water.vic.gov.au/how-much-water-was-available.php",
-]
+urls_to_parse = []
 
 
 # Web crawler to get all linked urls from a website.
@@ -15,33 +13,40 @@ urls_to_parse = [
 # TODO: Convert relative links to absolute links.
 # @refer https://stackoverflow.com/questions/59347372/how-extract-all-urls-in-a-website-using-beautifulsoup
 def get_url_list_from_site(homepage_url):
-    url = homepage_url
-    reqs = requests.get(url)
+    reqs = requests.get(homepage_url)
     soup = BeautifulSoup(reqs.text, "html.parser")
 
-    # urls = []
+    urls = []
     for link in soup.find_all("a"):
-        print(link.get("href"))
+        urls.append(link.get("href"))
+    return urls
 
 
-def get_url_list_from_file(filename = "input/urls.txt"):
+"""
+ def get_url_list_from_file(filename = "input/urls.txt"):
     with open(filename, 'r', encoding='UTF-8') as file:
     while line := file.readline():
         print(line.rstrip())
+"""
 
 
-# Renames URL to a compatible filename.
+# Renames URL to a Windows and Unix compatible filename.
 def rename_url_to_pdf(url):
     return url.replace("https://", "").replace("/", "-") + ".pdf"
 
 
 # Returns file path with directory
-# NOTE: Subdirectories not working with current version of pdfkit.
+# If the directory does not exist, creates it.
 def move_to_directory(file):
-    return file
+    path = "output/"
+    path_exists = os.path.exists(path)
+    if not path_exists:
+        os.makedirs(path)
+        print("Directory {} does not exist. Creating directory.".format(path))
+    return path + file
 
 
-def delay(seconds):
+def delay(seconds=1):
     print("Sleeping for {} seconds".format(seconds))
     time.sleep(seconds)
 
@@ -53,11 +58,14 @@ def main():
 
     config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
 
-    get_url_list("https://howmuch.water.vic.gov.au/")
+    urls_to_parse = get_url_list_from_site("https://howmuch.water.vic.gov.au/")
+    print(urls_to_parse)
 
     # Working:
     pdfkit.from_url(
-        "http://google.com", "test-passed-google-com.pdf", configuration=config
+        "http://google.com",
+        move_to_directory("_test-passed-with-google-com.pdf"),
+        configuration=config,
     )
 
     # Iteration working with delay.
@@ -69,7 +77,9 @@ def main():
             print("Succesfully parsed {}".format(link))
         except:
             print("Failed to parse {}".format(link))
-        delay(1)
+        delay(0.7)
+
+    print("Website to pdf parsing complete.")
 
 
 if __name__ == "__main__":
