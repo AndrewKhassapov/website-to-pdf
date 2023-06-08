0988d5349dbf6e6f3415fd9c8cad5e019c1a0473
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-06-09 01:10:58 +1000

    Created write URL list to .txt file
    + Created write URL list to .txt file
    + Test PASSED

--- website-to-pdf.py
+++ website-to-pdf.py
@@ -9,6 +9,7 @@ urls_to_parse = []
 
 # Web crawler to get all linked urls from a website.
 # Converts relative paths to absolute paths.
+# @param homepage_url: The url of the website to crawl. eg. https://www.example.com
 # @return list of urls
 # TODO: Add recursion to get all urls from a website.
 # @refer https://stackoverflow.com/questions/59347372/how-extract-all-urls-in-a-website-using-beautifulsoup
@@ -51,6 +52,14 @@ def get_url_list_from_file(filename="input/urls.txt"):
     return urls
 
 
+# Creates new file with URL list
+def set_url_list_to_file(urls, filename="input/urls_from_site.txt"):
+    with open(filename, "w", encoding="UTF-8") as file:
+        for url in urls:
+            file.write("%s\n" % url)
+    print("Succesfully created URL list {}".format(filename))
+
+
 # Renames URL to a Windows and Unix compatible filename.
 def rename_url_to_pdf(url):
     return url.replace("https://", "").replace("/", "-") + ".pdf"
@@ -58,8 +67,7 @@ def rename_url_to_pdf(url):
 
 # Returns file path with directory
 # If the directory does not exist, creates it.
-def move_to_directory(file):
-    path = "output/"
+def move_to_directory(file, path="output/"):
     path_exists = os.path.exists(path)
     if not path_exists:
         os.makedirs(path)
@@ -73,15 +81,17 @@ def delay(seconds=1):
 
 
 def main():
-    print("=== Parsing websites to pdf. ===")
+    print("=== Parsing websites to pdf. ===\n")
 
     path_wkhtmltopdf = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
     # options_wkhtmltopdf = {"enable-local-file-access": None}
 
     config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
 
-    urls_to_parse = get_url_list_from_site("https://howmuch.water.vic.gov.au/")
+    # urls_to_parse = get_url_list_from_site("https://howmuch.water.vic.gov.au/")
+    urls_to_parse = get_url_list_from_file()  # TEST
     print(urls_to_parse)
+    set_url_list_to_file(urls_to_parse)  # TEST
 
     # Working:
     pdfkit.from_url(
@@ -99,11 +109,10 @@ def main():
             print("Succesfully parsed {}".format(link))
         except:
             print("Failed to parse {}".format(link))
-        delay(0.7)
+        delay(0.1)
 
-    print("=== Website to pdf parsing complete. ===")
+    print("\n=== Website to pdf parsing complete. ===")
 
 
 if __name__ == "__main__":
-    # print(get_url_list_from_file())  # TEST
     main()
